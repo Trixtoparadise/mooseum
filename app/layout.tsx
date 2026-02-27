fadb1c5f0bf66bb3631da789994fae976333e3ec
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-02-27 13:18:43 +0200

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function DashboardLayout({
       <html lang="en" className={`${brownBagLunch.variable} ${gangOfThree.variable} ${cascadiaMono.className}`}>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="theme-color" content="#8B5DCF" />
         </head>
         <body>
           <AppRouterCacheProvider
