df53d505821f8f0311b7861088a56bbd65526271
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-03-05 12:27:39 +0200

    Updated navbar and footer

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -3,7 +3,7 @@ import { Public } from '@mui/icons-material';
 
 export default function Footer () {
     return (
-        <footer className="bg-primary py-2">
+        <footer className="bg-primary bg-pixel-grid py-2">
             <h6 className="text-[0.7rem] sm:text-[0.9rem] text-secondary text-center font-mono">
                 Public Domain <Public className='h-4! w-4!'/> 2026 Mooseum. Built by <Link href='https://github.com/Trixtoparadise' className='font-custom text-pink-200'>Trixtoparadise</Link>.
             </h6>
