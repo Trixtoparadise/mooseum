dd31be05af25320da8def8de793c696b862cb1ed
Hlamolo Mhlanga <102307608+Trixtoparadise@users.noreply.github.com>
2026-03-05 16:14:38 +0200

    Building home (#1)
    * Adding packages
    
    * Writing custom Carousel
    
    * Updated navbar and footer
    
    * adding border radius
    
    * Adding some styling
    
    * Some border radius
    
    * Adding some responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,94 @@
+'use client';
+import * as React from 'react';
+import EmblaCarousel from './ui/carousel/EmblaCarousel';
+import { Button, Stack } from '@mui/material';
+import { EmblaOptionsType } from 'embla-carousel';
+
 export default function Page() {
-  return <h1>Home Page</h1>
+  const images : string[] = [
+    "https://res.cloudinary.com/dzzwjwhfl/image/upload/v1772443386/Impression_Sunrise_ryao7n.jpg",
+    "https://res.cloudinary.com/dzzwjwhfl/image/upload/v1772443385/Bedroom_in_Arles_dbv1wu.jpg",
+    "https://res.cloudinary.com/dzzwjwhfl/image/upload/v1772443389/Constellations_series_zruyoe.jpg",
+    "https://res.cloudinary.com/dzzwjwhfl/image/upload/v1772527132/Sunflower_Quilting_Bee_at_Arles_twbyvs.jpg",
+    "https://res.cloudinary.com/dzzwjwhfl/image/upload/v1772443412/The_Artist_s_Garden_at_Eragny_ooirg6.jpg"
+  ]
+
+  const OPTIONS: EmblaOptionsType = { loop: true }
+
+  return (
+    <>
+      <div className='hidden sm:flex gap-x-20 items-center justify-center mx-10 mt-25'>
+        <div className='max-w-md -mt-10'>
+          <h1 className=' text-center align-middle font-sans font-bold text-[5.15rem]! text-primary text-base/16! mb-8'>
+            EXPLORE THE REALM OF INTERESTING IMAGINATIONS
+          </h1>
+          <p className='text-justify align-middle font-mono text-[0.9rem] mb-10 text-primary font-light'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+          <div className='flex justify-center'>
+            <Stack spacing={4} direction="row">
+              <Button 
+                variant='contained'
+                className='bg-primary! font-sans! text-[1.5rem]! text-base/8! px-12! py-3!'
+              >
+                A TRIP DOWN<br/>MEMORY LANE
+              </Button>
+              <Button 
+                variant='contained'
+                className='bg-primary! font-sans! text-[1.5rem]! text-base/8! px-10! py-3!'
+              >
+                CHECK OUT SOME<br/>EXHIBITIONS
+              </Button>
+            </Stack>
+          </div>
+        </div>
+        <div>
+          <EmblaCarousel 
+            slides={images} 
+            options={OPTIONS}
+            width={500}
+            height={500}
+            carouselHeight={30} 
+          />
+        </div>
+      </div>
+      <div className='sm:hidden justify-center mx-6 mt-25'>
+        <div className='max-w-md -mt-17'>
+          <h1 className=' text-center align-middle font-sans font-bold text-[3rem]! text-primary text-base/12! mb-6'>
+            EXPLORE THE REALM OF INTERESTING IMAGINATIONS
+          </h1>
+          <p className='text-justify align-middle font-mono text-[1rem] mb-8 text-primary font-light'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+        </div>
+        <div className='my-2'>
+          <EmblaCarousel 
+            slides={images} 
+            options={OPTIONS} 
+            width={360}
+            height={360}
+            carouselHeight={20}
+          />
+        </div>
+        <div className='flex justify-center mt-3 mb-5'>
+          <Stack spacing={4} direction="row">
+            <Button 
+              variant='contained'
+              className='bg-primary! font-sans! text-[1.3rem]! text-base/8! px-6! py-3!'
+            >
+              A TRIP DOWN MEMORY LANE
+            </Button>
+            <Button 
+              variant='contained'
+              className='bg-primary! font-sans! text-[1.3rem]! text-base/8! px-6! py-3!'
+            >
+              CHECK OUT SOME EXHIBITIONS
+            </Button>
+          </Stack>
+        </div>
+      </div>
+    </>
+  )
 }
--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,14 +1,12 @@
 "use client";
 import * as React from 'react';
 import Link  from 'next/link';
-import { useRef } from 'react';
 import { Menu, LightMode, Palette, Man, ViewTimeline} from '@mui/icons-material';
 import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
 
 
 export default function Navbar () {
-    const menuDetailsRef = useRef<HTMLDetailsElement>(null);
     const [open, setOpen] = React.useState(false);
 
     interface Route {
@@ -21,11 +19,6 @@ export default function Navbar () {
         { path: "/artworks", name: "ARTWORKS" },
         { path: "/movements", name: "MOVEMENTS" },
     ];
-
-    const handleLinkClick = () => {
-        if (menuDetailsRef.current)
-            menuDetailsRef.current.removeAttribute('open');
-    }
     
     const toggleDrawer = (newOpen: boolean) => () => {
         setOpen(newOpen);
@@ -38,7 +31,8 @@ export default function Navbar () {
                 slotProps={{
                     paper: {
                         sx: {
-                            backgroundColor: '#360185'
+                            backgroundColor: '#360185',
+                            backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='80' viewBox='0 0 80 80'%3E%3Cg fill='%23f5f5f5' fill-opacity='0.085'%3E%3Cpath fill-rule='evenodd' d='M11 0l5 20H6l5-20zm42 31a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM0 72h40v4H0v-4zm0-8h31v4H0v-4zm20-16h20v4H20v-4zM0 56h40v4H0v-4zm63-25a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM53 41a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-30 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-28-8a5 5 0 0 0-10 0h10zm10 0a5 5 0 0 1-10 0h10zM56 5a5 5 0 0 0-10 0h10zm10 0a5 5 0 0 1-10 0h10zm-3 46a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM21 0l5 20H16l5-20zm43 64v-4h-4v4h-4v4h4v4h4v-4h4v-4h-4zM36 13h4v4h-4v-4zm4 4h4v4h-4v-4zm-4 4h4v4h-4v-4zm8-8h4v4h-4v-4z'/%3E%3C/g%3E%3C/svg%3E");`
                         }
                     }
                 }}
@@ -48,7 +42,7 @@ export default function Navbar () {
                     <List>
                         {routes.map((item) => (
                             <ListItem key={item.name} disablePadding>
-                                <ListItemButton className='bg-shade! my-0.5!'>
+                                <ListItemButton className='bg-shade! rounded-sm! my-1! mx-1.5!'>
                                     <ListItemIcon className='text-secondary!'>
                                         {item.name == "ARTISTS" ? <Man /> : item.name == "ARTWORKS" ? <Palette /> : <ViewTimeline />}
                                     </ListItemIcon>
@@ -63,12 +57,11 @@ export default function Navbar () {
     }
 
     return (
-        <header className='mb-0 px-3! sm:px-15! font-sans'>
-            <nav className='flex items-center justify-between gap-4 w-full'>
+        <header className='mb-0 px-3! sm:px-18! font-sans'>
+            <nav className='flex items-center justify-between w-full'>
                 <Link 
                     href="/" 
-                    onClick={handleLinkClick}
-                    className='hidden sm:flex text-[2.5rem] text-text-onDark hover:text-gray-200 transition-colors duration-200 -mt-2! py-3! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80 font-heading'
+                    className='hidden sm:flex text-[2.5rem] text-text-onDark hover:text-gray-200 transition-colors duration-200 -mt-2! py-3! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80'
                 >
                     MOOSEUM
                 </Link>
@@ -79,7 +72,7 @@ export default function Navbar () {
                             return (
                                 <li key={item.name}>
                                     <Link href={item.path} 
-                                        className='text-[1.6rem] text-text-onDark/85 hover:text-gray-200 transition-colors duration-200 py-1 focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80 font-heading'
+                                        className='text-[1.6rem] text-text-onDark/85 hover:text-gray-200 transition-colors duration-200 py-1 focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80'
                                     >
                                         {item.name}
                                     </Link>
@@ -89,7 +82,7 @@ export default function Navbar () {
                         <li>
                             <IconButton 
                                 onClick={toggleDrawer(true)}
-                                className='hidden sm:flex! hover:text-gray-200 transition-colors duration-200 mt-2! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80 font-heading' 
+                                className='hidden sm:flex! hover:text-gray-200 transition-colors duration-200 mt-2! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80' 
                             >
                                 <LightMode className='h-6! w-6! text-secondary '/>
                             </IconButton>
@@ -99,7 +92,7 @@ export default function Navbar () {
 
                 <IconButton 
                     onClick={toggleDrawer(true)}
-                    className='sm:hidden! hover:text-gray-200 transition-colors duration-200 -m-2.5! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80 font-heading' 
+                    className='sm:hidden! -m-2.5!' 
                 >
                     <Menu className='h-9.5! w-9.5! text-secondary' />
                 </IconButton>
@@ -108,15 +101,14 @@ export default function Navbar () {
                 
                 <Link 
                     href="/" 
-                    onClick={handleLinkClick}
-                    className='sm:hidden text-[2.3rem] text-text-onDark hover:text-gray-200 transition-colors duration-200 -mt-2! py-3! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80 font-heading'
+                    className='sm:hidden text-[2.3rem] -mt-2! py-3!'
                 >
                     MOOSEUM
                 </Link>
                 
                 <IconButton 
                     onClick={toggleDrawer(true)}
-                    className='sm:hidden! hover:text-gray-200 transition-colors duration-200 -m-2.5! focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80 font-heading' 
+                    className='sm:hidden! -m-2.5!' 
                 >
                     <LightMode className='h-8! w-8! text-secondary '/>
                 </IconButton>
