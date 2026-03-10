a6ab8b591056b1e16ac284fa1a01038ac246eff0
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-03-10 07:41:18 +0200

    Adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function DashboardLayout({
       <html lang="en" className={`${brownBagLunch.variable} ${gangOfThree.variable} ${cascadiaMono.className}`}>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="icon" href='/favicon.ico' sizes='any' />
         </head>
         <body>
           <AppRouterCacheProvider
