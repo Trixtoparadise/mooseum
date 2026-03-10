939eeafdd11c0d0deb5edde1e2269ed8d6997023
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-03-10 10:43:41 +0200

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import * as React from 'react';
 import Carousel from './ui/carousel/Carousel';
 import { Button, Stack } from '@mui/material';
 import { EmblaOptionsType } from 'embla-carousel';
