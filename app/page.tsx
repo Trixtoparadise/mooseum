207b46cf7bc14d2e436baf26f69f42359d0756f4
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-04-17 08:40:46 +0200

    Intergrating buttons wih Next Link component

--- app/page.tsx
+++ app/page.tsx
@@ -16,76 +16,78 @@ export default function Page() {
 
   return (
     <div className='flex grow justify-center'>
-      <div className='hidden sm:flex gap-x-10 md:gap-x-20 items-center sm:justify-center mx-10 mt-25 mb-10'>
-        <div className='md:max-w-md -mt-10'>
-          <h1 className=' text-center align-middle font-sans font-bold text-[4rem]! md:text-[5.15rem]!  text-primary text-base/14 md:text-base/16! mb-8'>
-            EXPLORE THE REALM OF INTERESTING IMAGINATIONS
-          </h1>
-          <p className='text-center align-middle font-mono md:text-[0.9rem] mb-5 text-primary font-light'>
-            Read about your favourite artist, discover new artworks and educate your self about
-            movements spanning almost a millenium.
-          </p>
-          <div className='flex justify-center my-10'>
-            <Stack spacing={4} direction="row">
-              <Button 
-                variant='contained'
-                className='bg-primary! font-sans! text-[1.4rem]! md:text-[1.5rem]! text-base/8! md:text-base/8! px-8! md:px-12! py-3!'
-              >
-                <Link href="/timeline#top">
-                  A TRIP DOWN<br/>MEMORY LANE
-                </Link>
-              </Button>
-              <Button 
-                variant='contained'
-                className='bg-primary! font-sans! text-[1.4rem]! md:text-[1.5rem]! text-base/8! md:text-base/8! px-5! md:px-10! py-3!'
-              >
-                CHECK OUT SOME<br/>EXHIBITIONS
-              </Button>
-            </Stack>
-          </div>
-        </div>
-        <div className='min-w-3xs'>
-          <Carousel 
-            slides={images} 
-            options={OPTIONS}
-          />
-        </div>
-      </div>
-      <div className='sm:hidden justify-center mx-6 mt-25 mb-10'>
-        <div className='-mt-17'>
-          <h1 className=' text-center align-middle font-sans font-bold text-[3rem]! text-primary text-base/12! mb-6'>
-            EXPLORE THE REALM OF INTERESTING IMAGINATIONS
-          </h1>
-          <p className='text-center align-middle font-mono text-[1rem] mb-8 text-primary font-light'>
-            Read about your favourite artists, discover new artworks and 
-            educate your self about movements spanning almost a millenium.
-          </p>
-        </div>
-        <div className='my-2'>
-          <Carousel 
-            slides={images} 
-            options={OPTIONS} 
-          />
-        </div>
-        <div className='flex justify-center mt-3'>
-          <Stack spacing={4} direction="row">
-            <Button 
-              variant='contained'
-              className='bg-primary! font-sans! text-[1.3rem]! text-base/8! px-6! py-3!'
-            >
-              <Link href="/timeline#top">
-                A TRIP DOWN MEMORY LANE
-              </Link>
-            </Button>
-            <Button 
-              variant='contained'
-              className='bg-primary! font-sans! text-[1.3rem]! text-base/8! px-6! py-3!'
-            >
-              CHECK OUT SOME EXHIBITIONS
-            </Button>
-          </Stack>
-        </div>
-      </div>
+		<div className='hidden sm:flex gap-x-10 md:gap-x-20 items-center sm:justify-center mx-10 mt-25 mb-10'>
+			<div className='md:max-w-md -mt-10'>
+				<h1 className=' text-center align-middle font-sans font-bold text-[4rem]! md:text-[5.15rem]!  text-primary text-base/14 md:text-base/16! mb-8'>
+					EXPLORE THE REALM OF INTERESTING IMAGINATIONS
+				</h1>
+				<p className='text-center align-middle font-mono md:text-[0.9rem] mb-5 text-primary font-light'>
+					Read about your favourite artist, discover new artworks and educate your self about
+					movements spanning almost a millenium.
+				</p>
+				<div className='flex justify-center my-10'>
+					<Stack spacing={4} direction="row">
+					<Button 
+						variant='contained'
+						href='/timeline#top'
+						LinkComponent={Link}
+						className='bg-primary! font-sans! text-[1.4rem]! md:text-[1.5rem]! text-base/8! md:text-base/8! px-8! md:px-12! py-3!'
+					>
+						A TRIP DOWN<br/>MEMORY LANE
+					</Button>
+					<Button 
+						variant='contained'
+						LinkComponent={Link}
+						className='bg-primary! font-sans! text-[1.4rem]! md:text-[1.5rem]! text-base/8! md:text-base/8! px-5! md:px-10! py-3!'
+					>
+						CHECK OUT SOME<br/>EXHIBITIONS
+					</Button>
+					</Stack>
+				</div>
+			</div>
+			<div className='min-w-3xs'>
+			<Carousel 
+				slides={images} 
+				options={OPTIONS}
+			/>
+			</div>
+		</div>
+		<div className='sm:hidden justify-center mx-6 mt-25 mb-10'>
+			<div className='-mt-17'>
+				<h1 className=' text-center align-middle font-sans font-bold text-[3rem]! text-primary text-base/12! mb-6'>
+					EXPLORE THE REALM OF INTERESTING IMAGINATIONS
+				</h1>
+				<p className='text-center align-middle font-mono text-[1rem] mb-8 text-primary font-light'>
+					Read about your favourite artists, discover new artworks and 
+					educate your self about movements spanning almost a millenium.
+				</p>
+			</div>
+			<div className='my-2'>
+				<Carousel 
+					slides={images} 
+					options={OPTIONS} 
+				/>
+			</div>
+			<div className='flex justify-center mt-3'>
+			<Stack spacing={4} direction="row">
+				<Button 
+					variant='contained'
+					href='/timeline#top'
+					LinkComponent={Link}
+					className='bg-primary! font-sans! text-[1.3rem]! text-base/8! text-center! px-8! py-3!'
+				>
+					A TRIP DOWN MEMORY LANE
+				</Button>
+				<Button 
+					variant='contained'
+					LinkComponent={Link}
+					className='bg-primary! font-sans! text-[1.3rem]! text-base/8! text-center! px-6! py-3!'
+				>
+					CHECK OUT SOME EXHIBITIONS
+				</Button>
+			</Stack>
+			</div>
+		</div>
     </div>
   )
 }
