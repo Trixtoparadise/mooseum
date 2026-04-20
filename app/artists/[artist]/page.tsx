52a3907a0127588daf19d7d36ae37f1647d1d062
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-04-20 09:59:07 +0200

    Adding dark mode to the 'artist' page

--- app/artists/[artist]/page.tsx
+++ app/artists/[artist]/page.tsx
@@ -65,7 +65,7 @@ export default async function ArtistPage({
 
     return (
         <div className='mx-6 sm:mx-18'>
-            <h1 className='font-sans font-bold text-[2.7rem] sm:text-5xl text-primary mt-5 sm:mt-15 mb-4 sm:mb-10'>
+            <h1 className='font-sans font-bold text-[2.7rem] sm:text-5xl text-primary-light! dark:text-primary-dark! mt-5 sm:mt-15 mb-4 sm:mb-10'>
                 About artist:
             </h1>
             <div className=' flex flex-col md:flex-row items-stretch gap-6'>
@@ -79,15 +79,15 @@ export default async function ArtistPage({
                     />
                 </div>
 
-                <div className='flex-1 flex flex-col gap-2 p-4 max-w-full sm:max-w-full md:max-w-3/4 lg:max-w-2/3 xl:max-w-1/2 2xl:max-w-1/3 bg-primary/10 rounded-md'>
+                <div className='flex-1 flex flex-col gap-2 p-4 max-w-full sm:max-w-full md:max-w-3/4 lg:max-w-2/3 xl:max-w-1/2 2xl:max-w-1/3 bg-primary-light/10 dark:bg-primary-dark/10 rounded-md'>
                     <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 mx-0 sm:mx-4'>
-                        <p className='px-4 py-1 sm:py-3 text-primary rounded-sm overflow-hidden'>
+                        <p className='px-4 py-1 sm:py-3 text-primary-light dark:text-primary-dark rounded-sm overflow-hidden'>
                             <strong className='block truncate font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold -line-'>Name:</strong><br/>
                             <span className="block wrap-break-word text-md sm:text-lg md:text-md lg:text-lg font-light -mt-6">
                                 {artistData.name}
                             </span>
                         </p>
-                        <p className='px-4 py-1 sm:py-3 text-primary rounded-sm overflow-hidden'>
+                        <p className='px-4 py-1 sm:py-3 text-primary-light dark:text-primary-dark rounded-sm overflow-hidden'>
                             <strong className="block truncate font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold ">Nationality:</strong><br/> 
                             <span className="block wrap-break-word text-md sm:text-lg md:text-md lg:text-lg font-light -mt-6">
                                 {artistData.nationality}
@@ -95,19 +95,19 @@ export default async function ArtistPage({
                         </p>
 
                     </div>
-                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary rounded-sm text-md sm:text-lg md:text-md lg:text-lg font-light'>
+                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary-light dark:text-primary-dark rounded-sm text-md sm:text-lg md:text-md lg:text-lg font-light'>
                         <strong className='font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold'>Movements:</strong><br/>{movements.map((item) => item.name).join(", ")}
                     </p>
-                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary rounded-sm text-md sm:text-lg md:text-md lg:text-lg font-light'>
+                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary-light dark:text-primary-dark rounded-sm text-md sm:text-lg md:text-md lg:text-lg font-light'>
                         <strong className='font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold'>Years:</strong><br/> {artistData.years}
                     </p>
-                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary rounded-sm'>
+                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary-light dark:text-primary-dark rounded-sm'>
                         <strong className='font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl'>Biography:</strong><br/> 
                         <span className='text-md sm:text-lg md:text-md lg:text-lg font-light leading-relaxed'>{artistData.biography}</span>
                     </p>
                 </div>
             </div>
-            <h1 className='font-sans font-bold text-[2.5rem] sm:text-5xl text-primary mt-5 sm:mt-15 mb-4 sm:mb-10'>
+            <h1 className='font-sans font-bold text-[2.5rem] sm:text-5xl text-primary-light dark:text-primary-dark mt-5 sm:mt-15 mb-4 sm:mb-10'>
                 Famous Artworks:
             </h1>
             <ImageMasonry 
