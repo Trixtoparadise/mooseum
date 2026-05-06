6799f6e3085d4ee99c34323eb30fd405726fb4d4
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-05-06 07:45:19 +0200

    adding font icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default function DashboardLayout({
 					<head>
 						<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 						<link rel="icon" href='/favicon.ico' sizes='any' />
+						<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 					</head>
 					<body className="bg-pixel-grid">
 							<AppRouterCacheProvider
