dcbb912628f996133b0367d7ad8f936b6f365cf8
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-05-20 12:48:07 +0200

    adding responsiveness to the text

--- app/artists/[artist]/page.tsx
+++ app/artists/[artist]/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
+import Box from '@mui/material/Box';
 import ImageMasonry from '@/app/ui/Masonry';
+import Typography from '@mui/material/Typography';
 
 interface ArtistData {
   id: string;
@@ -64,55 +66,44 @@ export default async function ArtistPage({
     );
 
     return (
-        <div className='mx-6 sm:mx-18'>
-            <h1 className='font-sans font-bold text-[2.7rem] sm:text-5xl text-primary-light! dark:text-primary-dark! mt-5 sm:mt-15 mb-4 sm:mb-10'>
-                About artist:
-            </h1>
-            <div className=' flex flex-col md:flex-row items-stretch gap-6'>
-                <div className='w-full md:w-1/3 lg:w-1/4 shrink-0 '>
+        <Box className='mx-6! sm:mx-18!'>
+            <Typography className='font-mono! font-extralight! text-[2rem]! sm:text-4xl! text-center! sm:text-start! text-primary-light! dark:text-primary-dark! mt-5! sm:mt-15! mb-4! sm:mb-10!'>
+                 {artistData.name}
+            </Typography>
+            <Box className='flex! flex-col! md:flex-row! items-stretch! gap-6! mb-5! sm:mb-10!'>
+                <Box className='w-full! md:w-1/3! lg:w-1/4! shrink-0!'>
                     <Image
                         src={artistData.imageUrl}
                         alt={artistData.name}
                         width={600}
                         height={400}
-                        className='rounded-md h-full object-cover'
+                        className='rounded-md! h-full! object-cover!'
                     />
-                </div>
+                </Box>
 
-                <div className='flex-1 flex flex-col gap-2 p-4 max-w-full sm:max-w-full md:max-w-3/4 lg:max-w-2/3 xl:max-w-1/2 2xl:max-w-1/3 bg-primary-light/10 dark:bg-primary-dark/10 rounded-md'>
-                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 mx-0 sm:mx-4'>
-                        <p className='px-4 py-1 sm:py-3 text-primary-light dark:text-primary-dark rounded-sm overflow-hidden'>
-                            <strong className='block truncate font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold -line-'>Name:</strong><br/>
-                            <span className="block wrap-break-word text-md sm:text-lg md:text-md lg:text-lg font-light -mt-6">
-                                {artistData.name}
-                            </span>
-                        </p>
-                        <p className='px-4 py-1 sm:py-3 text-primary-light dark:text-primary-dark rounded-sm overflow-hidden'>
-                            <strong className="block truncate font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold ">Nationality:</strong><br/> 
-                            <span className="block wrap-break-word text-md sm:text-lg md:text-md lg:text-lg font-light -mt-6">
-                                {artistData.nationality}
-                            </span>
-                        </p>
+                <Box className='flex-1! flex! flex-col! gap-2! p-4! max-w-full! sm:max-w-full! md:max-w-3/4! lg:max-w-2/3! xl:max-w-1/2! 2xl:max-w-1/3! bg-primary-light/10! dark:bg-primary-dark/10! rounded-md!'>
+                    <Typography className='font-mono! px-4! pb-1! sm:px-0! sm:pb-3! sm:mx-4! text-primary-light! dark:text-primary-dark! text-[1rem]! sm:text-md! font-light!'>
+                        <strong className='font-sans! text-[2rem]! sm:text-4xl/12!'>Nationality</strong><br/>{artistData.nationality}
+                    </Typography>
+                    <Typography className='font-mono! px-4! pb-1! sm:px-0! sm:pb-3 sm:mx-4! text-primary-light! dark:text-primary-dark! text-[1rem]! sm:text-md! font-light!'>
+                        <strong className='font-sans! text-[2rem]! sm:text-4xl/12!'>Movements</strong><br/>{movements.map((item) => item.name).join(", ")}
+                    </Typography>
+                    <Typography className='font-mono! px-4! py-1! sm:px-0! sm:py-3! sm:mx-4! text-primary-light! dark:text-primary-dark! text-[1rem]! sm:text-md! font-light!'>
+                        <strong className='font-sans! text-[2rem]! sm:text-4xl/12!'>Years</strong><br/> {artistData.years}
+                    </Typography>
+                    <Typography className='font-mono! px-4! pb-2! sm:px-0! sm:pb-3! sm:mx-4! text-primary-light! dark:text-primary-dark! text-[1rem]! sm:text-md! font-light!'>
+                        <strong className='font-sans! text-[2rem]! sm:text-4xl/13!'>Biography</strong><br/> {artistData.biography}
+                    </Typography>
+                </Box>
+            </Box>
 
-                    </div>
-                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary-light dark:text-primary-dark rounded-sm text-md sm:text-lg md:text-md lg:text-lg font-light'>
-                        <strong className='font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold'>Movements:</strong><br/>{movements.map((item) => item.name).join(", ")}
-                    </p>
-                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary-light dark:text-primary-dark rounded-sm text-md sm:text-lg md:text-md lg:text-lg font-light'>
-                        <strong className='font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl font-bold'>Years:</strong><br/> {artistData.years}
-                    </p>
-                    <p className='px-4 py-1 sm:py-3 mx-0 sm:mx-4 text-primary-light dark:text-primary-dark rounded-sm'>
-                        <strong className='font-sans text-[2rem] sm:text-4xl md:text-3xl lg:text-4xl'>Biography:</strong><br/> 
-                        <span className='text-md sm:text-lg md:text-md lg:text-lg font-light leading-relaxed'>{artistData.biography}</span>
-                    </p>
-                </div>
-            </div>
-            <h1 className='font-sans font-bold text-[2.5rem] sm:text-5xl text-primary-light dark:text-primary-dark mt-5 sm:mt-15 mb-4 sm:mb-10'>
-                Famous Artworks:
-            </h1>
+            <Typography className='font-mono! font-extralight! text-[2rem]! sm:text-4xl! text-center! sm:text-start! text-primary-light! dark:text-primary-dark! mt-5! sm:mt-15! mb-4! sm:mb-10!'>
+                Famous Artworks
+            </Typography>
+
             <ImageMasonry 
                 data={artworksData}
             />
-        </div>
+        </Box>
     )
 }
