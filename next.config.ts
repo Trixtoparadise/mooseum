eb9007f3ba4072bf7d693dd74ec7db38e799d27e
Trixtoparadise <Hlamolo.mhlanga7@gmail.com>
2026-05-26 12:11:02 +0200

    Updating next.config.ts to handle SVG assets as componets.

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,46 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/dzzwjwhfl/**'
+      { 
+        protocol: 'https', 
+        hostname: '://cloudinary.com', 
+        pathname: '/dzzwjwhfl/**' 
       }
     ]
-  }
+  },
+  
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
